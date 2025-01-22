97a15fa913a21278124a0cccac155e116a42ff6d
carloscp-11 <cpa0005@alu.medac.es>
2025-01-22 16:29:51 +0100

    Mino´s
    Mino Movimiento, MinoL1, MinoL2, MinoT

--- src/mino/Mino.java
+++ src/mino/Mino.java
@@ -25,12 +25,42 @@ public class Mino {
 	}
 
 	public void setXY(int x, int y) {}
-	public void updateXY(int direction) {}
+	public void updateXY(int direction) {
+		
+		this.direction = direction;
+		tempB[0].x = b[0].x;
+    	tempB[0].y = b[0].y;
+    	tempB[1].x = b[1].x;
+    	tempB[1].y = b[1].y;
+    	tempB[2].x = b[2].x;
+    	tempB[2].y = b[2].y;
+    	tempB[3].x = b[3].x;
+    	tempB[3].y = b[3].y;
+	}
+	public void getDirection1() {}
+	public void getDirection2() {}
+	public void getDirection3() {}
+	public void getDirection4() {}
 	public void update() {
 		
 		//Movement
+		if(KeyHandler.upPressed) {
+			switch(direction) {
+			case 1: getDirection2();break;
+			case 2: getDirection3();break;
+			case 3: getDirection4();break;
+			case 4: getDirection1();break;
+			}
+			KeyHandler.upPressed = false;
+		}
 		if(KeyHandler.downPressed) {
 			
+			b[0].y += Block.SIZE;
+			b[1].y += Block.SIZE;
+			b[2].y += Block.SIZE;
+			b[3].y += Block.SIZE;
+		    autoDropCounter = 0;  //AL DESPLAZARSE HACIA ABAJO SE REINICIA EL AUTODROP
+		    KeyHandler.downPressed = false;
 		}
 		
 		autoDropCounter++; //sube con cada frame
