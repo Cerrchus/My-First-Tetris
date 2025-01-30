ee0a0529ecca98634817cdb6bb8bd33cc3a57528
jorge2004sm <jorgesanchezmarin04@gmail.com>
2025-01-30 15:20:42 +0100

    sound fixed

--- src/main/Sound.java
+++ src/main/Sound.java
@@ -12,6 +12,7 @@ public class Sound {
 
     public Sound(){
         url[0]= getClass().getResource("white-labyrinth-active.wav");
+//        url[0]= getClass().getResource("Tetris.wav");
         url[1]= getClass().getResource("delete line.wav");
         url[2]= getClass().getResource("gameover.wav");
         url[3]= getClass().getResource("rotation.wav");
